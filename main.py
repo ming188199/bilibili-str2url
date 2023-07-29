6d9d5cce4a27e519915e0940ffa0cf627fe65ecf
ming188199 <63723603+ming188199@users.noreply.github.com>
2023-07-29 23:01:08 +0800

    添加下载字幕文件

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ def download_str(url):
     with open("str.json", "wb") as f:
         f.write(response.content)
 
-# url_str = input('字幕网址')
+url_str = input('字幕网址') # 可临时下载
 # just for test
-url_str = 'https://aisubtitle.hdslb.com/bfs/ai_subtitle/prod/20624185337534032221caeb77d0d7ce61f5dbf8f14d4de5?auth_key=1690605908-29d09c3e41514887882d035c0ac7cf38-0-524c9b64862d4efacc5d0ed207f8fa55'
-# download_str(url_str)
-# 无法抓取，只能手动下载了。
+# url_str = 'https://aisubtitle.hdslb.com/bfs/ai_subtitle/prod/20624185337534032221caeb77d0d7ce61f5dbf8f14d4de5?auth_key=1690605908-29d09c3e41514887882d035c0ac7cf38-0-524c9b64862d4efacc5d0ed207f8fa55'
+download_str(url_str)
+
 
 import json
 with open('str.json') as f:
